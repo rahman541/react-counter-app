551087f3cf1a1214e399e306249028dc122683d5
Rahman <rahmanromli@gmail.com>
2018-09-07 05:36:19 +0800

    Add Counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+export class Counter extends Component {
+	state = {}
+	render() {
+		return <h1>Hello world</h1>
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import {Counter} from './components/counter'
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 registerServiceWorker();
