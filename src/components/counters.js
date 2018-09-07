f5dc61bb4e6071f2df82d7b5663983bc697dcaf2
Rahman <rahmanromli@gmail.com>
2018-09-07 21:07:47 +0800

    Add counters component for placehold counter comp

---
+++ src/components/counters.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import {Counter} from './counter'
+
+export default class Counters extends Component {
+	state={
+		counters: [
+			{id: 1, value: 0},
+			{id: 2, value: 1},
+			{id: 3, value: 2},
+			{id: 4, value: 3},
+		]
+	}
+	render() {
+		const {counters} = this.state
+		return (
+			<React.Fragment>
+				{ counters.map(counter => <Counter key={counter.id}/>) }
+			</React.Fragment>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import {Counter} from './components/counter'
+import Counters from './components/counters'
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counters />, document.getElementById('root'));
 registerServiceWorker();
