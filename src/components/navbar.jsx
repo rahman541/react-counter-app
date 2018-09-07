f41f18b2dedd50695fc870386a657a2c26913b48
Rahman <rahmanromli@gmail.com>
2018-09-08 00:07:41 +0800

    Use function instead of class for simple render

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 
-export default class NavBar extends Component {
-	render() {
-		return (
-			<nav className="navbar navbar-light bg-light">
-			  <a className="navbar-brand" href="#">
-			  	Navbar <span className="span badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-		  	</a>
-			</nav>
-		)
-	}
+// Stateless Functional Component
+const Navbar = props => {
+	const {totalCounters} = props
+	return (
+		<nav className="navbar navbar-light bg-light">
+		  <a className="navbar-brand" href="#">
+		  	Navbar <span className="span badge badge-pill badge-secondary">{totalCounters}</span>
+	  	</a>
+		</nav>
+	)
 }
+
+export default Navbar
